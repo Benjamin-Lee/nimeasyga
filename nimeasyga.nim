77b3a8fa5c0acd8bdbf3cb087657d963963334bc
Benjamin Lee <benjamindlee@me.com>
2020-01-16 21:40:43 -0700

    Add a bunch of missing methods

--- nimeasyga.nim
+++ nimeasyga.nim
@@ -2,35 +2,45 @@ import random
 
 ## A simple, easy to use package for genetic algorithms in Nim.
 
-proc createIndividual*(genomeSize: Positive): seq[bool] =
+proc createIndividual*[T](data: T): seq[bool] =
   ## The default individual creation method.
-  ## Creates a random seq of
-  result = newSeq[bool](genomeSize)
-  for index in 0..<genomeSize:
+  ## Creates a seq of randomly chosen bools of the same length of data.
+  result = newSeq[bool](data.len)
+  for index in 0..<data.len:
     result[index] = random.sample([true, false])
 
-proc crossover*[T](parent_1: seq[bool], parent_2: seq[bool]): (seq[bool], seq[bool]) =
-  ## The default crossover function. Takes two parents and chooses a random index at which to recombine them.
-  # TODO: placeholder
-  return (parent_1, parent_2)
+proc crossover*[T](parent_1: seq[T], parent_2: seq[T]): (seq[T], seq[T]) =
+  ## The default crossover function.
+  ## Takes two parents and chooses a random index at which to recombine them.
+  let idx = rand(0..<parent_1.high)
+  return (parent_1[0..idx] & parent_2[idx+1..parent_2.high], parent_2[0..idx] &
+      parent_1[idx+1..parent_1.high])
 
 proc mutate*(individual: seq[bool]): seq[bool] =
-  ## Randomly flip a bit in the individual's genome.
+  ## The default mutation function when individuals are represented as `seq[bool]`.
+  ## Randomly flips a bit in the individual's genome.
   result = deepCopy(individual)
   let idx = rand(0..result.high)
   result[idx] = not result[idx]
 
-proc randomSelection*[T](population: seq[T]): T = population[0] # TODO: placeholder
-proc tournamentSelection*[T](population: seq[T],
-        tournamentSize: Positive = 2) = population[0] # TODO: placeholder
+proc randomSelection*[T](population: seq[T]): T =
+  return random.sample(population)
 
-proc geneticAlgorithm*[T](data: seq[T], fitness: proc(individual: seq[bool], data: seq[T]): float,
+proc tournamentSelection*[T](population: seq[T]): T =
+  return population[0] # TODO: placeholder
+
+proc geneticAlgorithm*[T, I](data: T, fitness: proc(individual: I, data: T): float,
                           generations: Positive = 2,
                           populationSize: Positive = 3,
                           crossoverRate: range[0.0..1.0] = 0.5,
                           mutationRate: range[0.0..1.0] = 0.5,
                           elitism = true,
-                          createIndividual = createIndividual,
+                          createIndividual: proc(data: T): I = createIndividual,
+                          mutate: proc(individual: I): I = mutate,
+                          crossover: proc(parent_1: I, parent_2: I): (I,
+                              I) = crossover,
+                          selection: proc(population: seq[
+                              I]): I = tournamentSelection,
                           seed: int64 = 0): float =
   ## The main algorithm to run the genetic algorithm.
   ##
@@ -46,17 +56,18 @@ proc geneticAlgorithm*[T](data: seq[T], fitness: proc(individual: seq[bool], dat
     randomize(seed)
 
   # create the intial population
-  var population = newSeq[seq[bool]](populationSize)
+  var population = newSeq[I](populationSize)
   for i in 0..<populationSize:
-    population[i] = createIndividual(data.len)
+    population[i] = createIndividual(data)
+  var nextPopulation = newSeq[I](populationSize) # to hold the next generation
   echo population
 
-  for generation in 0..<generations:
-    var fitnesses = newSeq[float](populationSize)
-    for individual in 0..<populationSize:
-      fitnesses[individual] = fitness(population[individual], data)
+  var fitnesses = newSeq[float](populationSize)
 
-    var newPopulation = newSeq[seq[bool]](populationSize)
+  for generation in 0..<generations:
+
+    for idx, individual in population:
+      fitnesses[idx] = fitness(individual, data)
 
     # preserve the fittest individual, if requested
     if elitism:
@@ -71,9 +82,9 @@ proc geneticAlgorithm*[T](data: seq[T], fitness: proc(individual: seq[bool], dat
           fittestIndividualIdx = i
 
       # add it to the new population
-      newPopulation[0] = population[fittestIndividualIdx]
+      nextPopulation[0] = population[fittestIndividualIdx]
 
-    population = newPopulation
+    population = nextPopulation
 
     echo fitnesses
   return -1000.0 # TODO: placeholder
